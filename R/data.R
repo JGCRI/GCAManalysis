40203ccb610ca8f92a01e3dd90f0cd087d5a022c
Robert Link <robert.link@pnnl.gov>
2017-06-14 17:58:43 -0400

    Add population data set for testing, examples, and experimentation.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Population data from the GCAM reference scenario
+#'
+#' A dataset containing the population by region for the GCAM reference
+#' scenario.
+#'
+#' @format Data frame in long format with 672 rows and 3 variables:
+#' \describe{
+#'   \item{region}{GCAM region name.  There are 32 regions total}
+#'   \item{year}{Year of the data point}
+#'   \item{population}{Reference scenario population for the region and year.}
+#' }
+"population"
